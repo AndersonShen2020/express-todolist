ab9a81297774081c534b8d99ac07ee222deaa50b
AndersonShen <shen.andersonshen@gmail.com>
2023-05-19 16:42:35 +0800

    23205191642

--- app.js
+++ app.js
@@ -23,20 +23,20 @@ mongoose.connection.once('open', () => console.log('connected to database'));
 mongoose.connection.on('error', (error) => console.error('database error', error));
 
 // 新增 todos
-app.post("/todos", async (req, res) => {
-    console.log('----[POST] 開始---');
+app.post("/api/todos", async (req, res) => {
+    console.log('---- [POST] 開始 ----');
     try{
-        console.log('----[POST] 進入 try，開始設定 todo item---');
+        console.log('---- [POST] 進入 try，開始設定 todo item ----');
         const item = { 
             ...req.body,
             dueDate: Date.now(),
         }
-        console.log('----[POST] 設定 todo item 完畢，進入 new todolist(item)---');
+        console.log('---- [POST] 設定 todo item 完畢，進入 new todolist(item) ----');
         const todo = new todolist(item)
-        console.log('----[POST] 設定 new todolist(item)完畢，開始 await todo.save()---');
+        console.log('---- [POST] 設定 new todolist(item)完畢，開始 await todo.save() ----');
         const result = await todo.save()
 
-        console.log('----[POST] 開始傳輸 API 回應---');
+        console.log('---- [POST] 開始傳輸 API 回應 ----');
         res.set(header)
         res.send(`${result} 
         save 成功`)
@@ -44,11 +44,11 @@ app.post("/todos", async (req, res) => {
         const errMsg = `POST Error ${err}`
         res.send({message: errMsg})
     }
-    console.log('----[POST] 結束---');
+    console.log('----[POST] 結束 ----');
 })
 
 // 修改 todos
-app.patch("/todos/:id", async (req, res) => {
+app.patch("/api/todos/:id", async (req, res) => {
     const  {id: _id}  = req.params
     const item = { 
         ...req.body,
@@ -65,7 +65,7 @@ app.patch("/todos/:id", async (req, res) => {
 })
 
 // 刪除所有 todos
-app.delete("/todos", async (req, res) => {
+app.delete("/api/todos", async (req, res) => {
     try{
         const result = await todolist.deleteMany({})
         res.set(header)
@@ -76,7 +76,7 @@ app.delete("/todos", async (req, res) => {
 })
 
 // 刪除單筆 todos
-app.delete("/todos/:id", async (req, res) => {
+app.delete("/api/todos/:id", async (req, res) => {
     const  {id: _id}  = req.params
     try{
         const result = await todolist.findOneAndDelete({_id: _id })
@@ -88,8 +88,8 @@ app.delete("/todos/:id", async (req, res) => {
 })
 
 // 取得所有 todos
-app.get("/todos", async (req, res) => {
-    console.log('----[GET] 開始---');
+app.get("/api/todos", async (req, res) => {
+    console.log('---- [GET] 開始 ----');
     try{
         const result = await todolist.find({})
         res.set(header)
@@ -97,11 +97,11 @@ app.get("/todos", async (req, res) => {
     }catch{
         res.send({message: 'GET Error'})
     }
-    console.log('----[GET] 結束---');
+    console.log('---- [GET] 結束 ----');
 })
 
 // 取得單一 todos
-app.get("/todos/:id", async (req, res) => {
+app.get("/api/todos/:id", async (req, res) => {
     const  {id: _id}  = req.params
     try{
         const result = await todolist.findById(_id)
